448dcc27328d92bad14f1d478926ab88291f0025
RedPancake <2453882868@qq.com>
2025-10-19 14:40:55 +0800

    制作了最后一关的雏形，更改了透明背景相关设置
    111

---
+++ scripts/levels/level_end_manager.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+#被尘封的代码
+#把背景框调成透明
+#func per_pixel_transparency():
+	#ProjectSettings.set_setting("display/window/per_pixel_transparency/allowed",true)
+##显示-窗口-像素级透明
+	#ProjectSettings.set_setting("display/window/size/transparent",true)
+##显示-窗口-透明
+	#ProjectSettings.set_setting("rendering/viewport/transparent_background",true)
+##渲染-视口-透明背景
+#func _ready() -> void:
+	#per_pixel_transparency()
+	#print(ProjectSettings.get_setting("display/window/per_pixel_transparency/allowed"))
+	#print(ProjectSettings.get_setting("display/window/size/transparent"))
+	#print(ProjectSettings.get_setting("rendering/viewport/transparent_background"))
