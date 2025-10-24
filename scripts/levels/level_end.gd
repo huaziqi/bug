28ae9d46731c77a6b82a2e3944adf0fc2dbfdcb1
levoxacin <2707666576@qq.com>
2025-10-24 23:31:26 +0800

    随便改改

--- scripts/levels/level_end.gd
+++ scripts/levels/level_end.gd
@@ -3,10 +3,13 @@ extends LevelManager
 @export var pop:PackedScene
 @export var error:PackedScene
 func _ready() -> void:
+	$endflake.visible=false
 	$Steel_Pipe.freezing=true
 	$flake_fountain.visible=false
 	$hole_background.visible=false
 	$flake_fountain/AnimationPlayer.play("new_animation")
+	await get_tree().create_timer(0.5).timeout
+	hide_item($Steel_Pipe)
 	$camera_for_end/AnimationPlayer.play("far")
 	await $camera_for_end/AnimationPlayer.animation_finished
 	await get_tree().create_timer(1.0).timeout
@@ -26,7 +29,11 @@ func _ready() -> void:
 	$flake_fountain/AnimationPlayer.play("apear")
 	await $flake_fountain/AnimationPlayer.animation_finished
 	$flakes.play()
+	$endflake.visible=true
+	$endflake.modulate.a=0
+	$endflake/AnimationPlayer.play("apear")
 	$flake_fountain/AnimationPlayer.play("new_animation")
+	show_item($Steel_Pipe)
 	$Steel_Pipe.freezing=false
 func _process(_delta: float) -> void:
 	if $Steel_Pipe.global_position.y<-100 and not finished:
