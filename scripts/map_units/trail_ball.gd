d03bfe4721d2c7e0bd2017504964b0e0b2a67078
levoxacin <2707666576@qq.com>
2025-10-26 21:53:42 +0800

    魔改第四关

--- scripts/map_units/trail_ball.gd
+++ scripts/map_units/trail_ball.gd
@@ -1,10 +1,12 @@
 extends StaticBody2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var particles: GPUParticles2D = $GPUParticles2D
-
-var in_bomb : bool = false
-
+@export var plat:PackedScene
 func _ready() -> void:
+	modulate.a=0.8
+	var p=plat.instantiate()
+	p.global_position=global_position
+	get_parent().add_child(p)
 	var tween = create_tween()
 	get_tree().create_timer(0.7).timeout.connect(func():
 		random_scale_distortion()
@@ -14,7 +16,8 @@ func _ready() -> void:
 	 0.9).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 
 	get_tree().create_timer(1).timeout.connect(func():
-		bomb()
+		p.queue_free()
+		disappear()
 		)
 
 
@@ -28,30 +31,8 @@ func random_scale_distortion():
 	tween.tween_property(sprite_2d, "scale", target_scale, 0.5)
 
 
-func bomb():
-	particles.emitting = true
-	in_bomb = true
+func disappear():
 	get_tree().create_timer(0.3).timeout.connect(func():
 		
 		queue_free()
 		)
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if in_bomb and body.is_in_group("player"):
-		apply_explosion_force(body)
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
-
-func apply_explosion_force(body: Node2D):
-	var explosion_center = global_position
-	var body_position = body.global_position
-	var direction = (body_position - explosion_center).normalized()
-
-	var distance = explosion_center.distance_to(body_position)
-	var force_strength = 270.0  # 基础力的大小
-
-	if body is RigidBody2D:
-		body.apply_central_impulse(direction * force_strength)
